b6bd8a3f1bc98e7c3738b086449932da424386a4
MrGultekin <gultekin.de@gmail.com>
2022-05-09 15:42:45 +0200

    Services module work.

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,7 +7,7 @@ const Services = () => {
             <h5>What I Offer</h5>
             <h2>Services</h2>
 
-            <div className="container service__container">
+            <div className="container services__container">
                 <article className="service">
                     <div className="service__head">
                         <h3>UI/UX Design</h3>
