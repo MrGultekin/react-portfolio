34fc0dcb642094a77ddc5f0e684da942b5c239e4
MrGultekin <gultekin.de@gmail.com>
2022-05-10 15:05:24 +0200

    Footer.jsx done.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,33 @@
 import './footer.css'
+import {FaFacebookF} from "react-icons/fa";
+import {FiInstagram} from "react-icons/fi";
+import {IoLogoTwitter} from "react-icons/io";
 
 const Footer = () => {
     return (
-        <div> footer </div>
+        <div>
+            <footer>
+                <a href="#" className={'footer__logo'}>Mr.Rose</a>
+                <ul className="permalinks">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experince</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#testimonials">Testimonials</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+
+                <div className="footer__socials">
+                    <a href="https://www.facebook.com/"><FaFacebookF/></a>
+                    <a href="https://www.instagram.com/"><FiInstagram/></a>
+                    <a href="https://www.twitter.com/"> <IoLogoTwitter/></a>
+                </div>
+                <div className="footer__copyright">
+                    <small>&copy; 2023 Portfolio. All rights reserved.</small>
+                </div>
+            </footer>
+        </div>
     )
 
 }
