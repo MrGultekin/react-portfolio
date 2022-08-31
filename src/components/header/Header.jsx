5859773865017450691bc837def2ff04fd4df973
MrGultekin <gultekin.de@gmail.com>
2022-08-31 21:14:59 +0200

    pic added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.jpg'
+import ME from '../../assets/me.png'
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
